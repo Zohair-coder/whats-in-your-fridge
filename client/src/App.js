222510cb18af76dd0e187bf6fcfac723cf190ea7
zohair <Zohair.ul.hasan@gmail.com>
2021-10-20 23:34:10 -0400

    simplified react

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { apiResponse: "" };
@@ -20,14 +18,12 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.apiResponse}</p>
-      </div>
-    );
+    <div>
+      <h1>What's in your fridge?</h1>
+      <p className="App-intro">{this.state.apiResponse}</p>
+    </div>
+    
+    )
   }
 }
 
