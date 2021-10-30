41a1e9d63ee93da8e75d942462159e92e9cbac94
zohair <Zohair.ul.hasan@gmail.com>
2021-10-30 14:58:39 -0400

    generate recipe's

--- client/src/pages/results/Results.js
+++ client/src/pages/results/Results.js
@@ -1,12 +1,47 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import { useLocation } from "react-router-dom"
 
 function Results() {
+    
     let location = useLocation()
-    console.log(location.state.items)
+    const [recipes, setRecipes] = useState([])
+    
+    
+    useEffect(() => {
+        let ingredientNames = ""
+        location.state.items.forEach(ingredient => ingredientNames += ingredient.name + ",");
+        ingredientNames = ingredientNames.slice(0, -1); // remove the last comma
+
+        const params = new URLSearchParams({
+            apiKey: "37299eda2bf644f7a90f8edb1736f7d6",
+            ingredients: ingredientNames
+        })
+    
+        let base_url = "https://api.spoonacular.com/recipes/findByIngredients"
+        let url = base_url + "?" + params.toString()
+
+        let resp = axios({
+            url: url,
+            method: "get",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    
+        resp.then(r => {
+            setRecipes(r.data);
+        })
+
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+    console.log(recipes)
+
+    const recipeComponent = recipes.map(recipe => <h3>{recipe.title}</h3>)
+
     return (
         <div>
-            <h1>Results</h1>
+            <h1>Recipe's</h1>
+            {recipeComponent}
         </div>
     )
 }
